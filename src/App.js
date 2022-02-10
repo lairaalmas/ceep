a0a3216fec6b5cec414d56eb827394f9361a0c52
laira <laira@ime.usp.br>
2022-02-09 23:27:22 -0300

    padronizando a criacao de componentes usando classe

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { Component } from "react";
 import FormularioCadastro from "./components/FormularioCadastro.jsx";
 import ListaDeNotas from "./components/ListaDeNotas.jsx";
 
-function App() {
-  return (
-    // aqui vai um JSX
-    <section>
-      <FormularioCadastro/>
-      <ListaDeNotas/>
-    </section>
-  );
+class App extends Component {
+  render() {
+    return (
+      // aqui vai um JSX
+      <section>
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
+  }
 }
 
 export default App;
